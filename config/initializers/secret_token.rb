918474547fbcbf2b729b329479bac7e9673ba23b
JU An <andyjvan@gmail.com>
2015-01-26 13:53:03 +0800

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VMControl::Application.config.secret_key_base = '55706106c9f884e125df18944a70eff452bdb26d8d89bbc36447819a4a61c0515da1b2b6bc9c3d6b15ecc231e8e3c11dda5f32d06c97ccdf650fa51f47aaf0a8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VMControl::Application.config.session_store :cookie_store, key: '_VMControl_session'
