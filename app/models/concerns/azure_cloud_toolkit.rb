3c7acdaf92e7b539126c110e5c283dba5e9a3d30
JoeJ11 <andyjvan@hotmail.com>
2015-05-14 15:06:17 +0800

    Add some Azure features. Will continue later.

--- app/models/concerns/azure_cloud_toolkit.rb
+++ app/models/concerns/azure_cloud_toolkit.rb
@@ -9,6 +9,9 @@ module AzureCloudToolkit
     config.management_endpoint = 'https://management.core.windows.net'
   end
 
+  STORAGE_ACCOUNT_NAME = 'thumoocy2qawigx'
+  AFFINITY_GROUP = 'Wonderland'
+
   def self.included(base)
     base.extend(ClassMethods)
   end
@@ -25,6 +28,24 @@ module AzureCloudToolkit
   end
 
   def create_machine
+    setting = JSON.parse(self.setting)
+    config = self.cluster_configuration
+    machines = config.cluster_templates
+
+    net_name = create_virtual_network setting
+    setting[:virtual_net] = net_name
+    service = create_role setting, machines[0]
+    setting[:service] = service
+    for i in (1...machines.size) do
+      add_role setting, machines[i]
+    end
+    self.status = CloudToolkit::STATUS_ONPROCESS
+    self.specifier = service
+    self.save
+  end
+
+  # create role
+  def create_role(setting, machine)
     virtual_machine_service = Azure::VirtualMachineManagementService.new
     service_name = 'mooccluster' + random_string(8)
     while self.class.check_cloud_service_name service_name
@@ -32,45 +53,76 @@ module AzureCloudToolkit
       service_name = 'mooccluster' + random_string(8)
     end
 
-    puts 'Create machine...'
     params = {
-        :vm_name => 'remote_machine',
+        :vm_name => machine.name,
         :vm_user => 'azureuser',
-        :image => 'GraphMaster1',
+        :image => machine.image_id,
         :password => 'Thumooc_2015',
         :location => 'East US',
-        :affinity_group_name => 'Wonderland'
+        :affinity_group_name => AFFINITY_GROUP
     }
     options = {
-        :storage_account_name => 'thumoocy2qawigx',
+        :storage_account_name => STORAGE_ACCOUNT_NAME,
         # :winrm_transport => ['https','http'], #Currently http is supported. To enable https, set the transport protocol to https, simply rdp to the VM once VM is in ready state, export the certificate ( CN name would be the deployment name) from the certstore of the VM and install to your local machine and communicate WinRM via https.
         :cloud_service_name => service_name,
         :deployment_name => 'thu',
-        :tcp_endpoints => '80,3389:3390',
+        :tcp_endpoints => '80',
         # :private_key_file => 'c:/private_key.key', #required for ssh or winrm(https) certificate.
         # :certificate_file => 'c:/certificate.pem', #required for ssh or winrm(https) certificate.
         :ssh_port => 22,
-        :vm_size => 'Basic_A1', #valid choices are (Basic_A0,Basic_A1,Basic_A2,Basic_A3,Basic_A4,ExtraSmall,Small,Medium,Large,ExtraLarge,A5,A6,A7,A8,A9,Standard_D1,Standard_D2,Standard_D3,Standard_D4,Standard_D11,Standard_D12,Standard_D13,Standard_D14,Standard_DS1,Standard_DS2,Standard_DS3,Standard_DS4,Standard_DS11,Standard_DS12,Standard_DS13,Standard_DS14,Standard_G1,Standard_G2,Standard_G3,Standard_G4,Standard_G5)
-        :affinity_group_name => 'Wonderland',
-        :virtual_network_name => 'test_virtual_net',
-        :subnet_name => 'subnet-1',
+        :vm_size => machine.flavor_id, #valid choices are (Basic_A0,Basic_A1,Basic_A2,Basic_A3,Basic_A4,ExtraSmall,Small,Medium,Large,ExtraLarge,A5,A6,A7,A8,A9,Standard_D1,Standard_D2,Standard_D3,Standard_D4,Standard_D11,Standard_D12,Standard_D13,Standard_D14,Standard_DS1,Standard_DS2,Standard_DS3,Standard_DS4,Standard_DS11,Standard_DS12,Standard_DS13,Standard_DS14,Standard_G1,Standard_G2,Standard_G3,Standard_G4,Standard_G5)
+        :affinity_group_name => AFFINITY_GROUP,
+        :virtual_network_name => setting[:virtual_net],
+        :subnet_name => 'subnet',
         # :availability_set_name => 'availabiltyset1',
-        :reserved_ip_name => '192.168.0.2'
+        # :reserved_ip_name => '192.168.0.2'
     }
     virtual_machine_service.create_virtual_machine(params,options)
+    service_name
+  end
 
+  # Add role
+  def add_role(setting, machine)
+    virtual_machine_service = Azure::VirtualMachineManagementService.new
+    params = {
+        :vm_name => machine.name,
+        :cloud_service_name => setting[:service],
+        :vm_user => 'azureuser',
+        :image => machine.image_id,
+        :password => 'Password4test',
+    }
+    options = {
+        :storage_account_name => STORAGE_ACCOUNT_NAME,
+        # :winrm_transport => ['https','http'], #Currently http is supported. To enable https, set the transport protocol to https, simply rdp to the VM once VM is in ready state, export the certificate ( CN name would be the deployment name) from the certstore of the VM and install to your local machine and communicate WinRM via https.
+        :tcp_endpoints => '80',
+        # :private_key_file => 'c:/private_key.key', #required for ssh or winrm(https) certificate.
+        # :certificate_file => 'c:/certificate.pem', #required for ssh or winrm(https) certificate.
+        # :winrm_https_port => 5999,
+        # :winrm_http_port => 6999, #Used to open different powershell port
+        :vm_size => machine.flavor_id, #valid choices are (Basic_A0,Basic_A1,Basic_A2,Basic_A3,Basic_A4,ExtraSmall,Small,Medium,Large,ExtraLarge,A5,A6,A7,A8,A9,Standard_D1,Standard_D2,Standard_D3,Standard_D4,Standard_D11,Standard_D12,Standard_D13,Standard_D14,Standard_DS1,Standard_DS2,Standard_DS3,Standard_DS4,Standard_DS11,Standard_DS12,Standard_DS13,Standard_DS14,Standard_G1,Standard_G2,Standard_G3,Standard_G4,Standard_G5)
+        # :availability_set_name => 'availabiltyset'
+    }
+    virtual_machine_service.add_role(params, options)
   end
 
   # Create a new virtual network.
-  def create_virtual_network
+  def create_virtual_network(setting)
     virtual_net = Azure::VirtualNetworkManagementService.new
+    net_name = 'moocvpn' + random_string(8)
 
-    address_space = ['192.168.0.0/12']
-    subnets = [{:name => 'subnet-1',  :ip_address=>'192.168.0.0',  :cidr=>12}]
+    address_space = ['192.168.0.0/16']
+    subnets = [{:name => 'subnet',  :ip_address=>'192.168.0.0',  :cidr=>16}]
     # dns_servers = [{:name => 'dns-1',  :ip_address=>'30.30.30.1'}]
     options = {:subnet => subnets}
 
-    virtual_net.set_network_configuration('test_virtual_net', 'Wonderland', address_space, options)
+    virtual_net.set_network_configuration(net_name, AFFINITY_GROUP, address_space, options)
+    net_name
+  end
+
+  # Delete machine
+  def delete_machine
+    virtual_machine_service = Azure::VirtualMachineManagementService.new
+    virtual_machine_service.delete_virtual_machine('name', 'service')
   end
 
   def random_string(length)
